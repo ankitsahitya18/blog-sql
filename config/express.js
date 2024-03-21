f7e3c41ed361f4c795f70e3830be9b7da639592e
ankitsahitya <spark18@Spark18s-MacBook-Pro.local>
2024-03-21 18:23:38 +0530

    configs setup

--- config/express.js
+++ config/express.js
@@ -2,6 +2,5 @@ const path = require("path");
 const app = require(path.resolve("config/app"))
 
 app.listen(process.env.PORT, () => {
-    require('./swaggerConfig')(app);
     console.log(`Server started on PORT ${process.env.PORT}!`);
 })
